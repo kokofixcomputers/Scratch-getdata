4f8a16e5c80dc0c603153c7a92cc62143ab4e5fc
kokofixcomputers <koko@trashmail.se>
2023-06-22 21:44:47 -0700

    Create setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='kokofixcomputers',
     description='A Module to fetch scratch things from ScratchGetData',
-    packages=['module'],
+    packages=find_packages(),
     install_requires=['requests'],
 )
