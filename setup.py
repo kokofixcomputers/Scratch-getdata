c4410dfc005792892a05fa045b7bb6fbe0abf6cc
kokofixcomputers <koko@trashmail.se>
2023-06-22 21:43:43 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Scratch-GetData',
+    version='0.0.1',
+    author='kokofixcomputers',
+    description='A Module to fetch scratch things from ScratchGetData',
+    packages=['module'],
+    install_requires=['requests'],
+)
